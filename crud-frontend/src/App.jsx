cf50d7b985a6bc3e6b2a5f02fae719ba52f9d6ec
maubri96 <rauzav96@outlook.es>
2025-11-15 11:01:30 -0600

    Creacion de Component: Navbar y tableList-

--- crud-frontend/src/App.jsx
+++ crud-frontend/src/App.jsx
@@ -1,19 +1,15 @@
+import './App.css';
+import { NavBar } from './component/NavBar';
+import { TableList } from './component/TableList';
 
-import './App.css'
 
 function App() {
-
-
   return (
     <>
-      <h1 className='text-3xl font-bold underline'> 
-        Hello Word!
-      </h1>
-      <div>
-        <button className="btn btn-primary">Hola DaisyUI</button>
-      </div>
+      <NavBar />
+      <TableList></TableList>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ crud-frontend/src/component/TableList.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export const TableList = () => {
+
+    const clients = [
+        {id: 1 , name: "John Doe", email: "john.doe@gmail.com", job: "Developer", rate: "100", isactive: true },
+        {id: 2 , name: "John Doe1", email: "john1.doe@gmail.com", job: "Developer1", rate: "101", isactive: true },
+        {id: 3 , name: "John Doe2", email: "john2.doe@gmail.com", job: "Developer2", rate: "102", isactive: false }
+
+    ]
+
+    return (
+        <div className="overflow-x-auto mt-20">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Job</th>
+                        <th>Rate</th>
+                        <th>Isactive</th>
+                    </tr>
+                </thead>
+                <tbody className='hover'>
+
+                {clients.map(client => (
+                    <tr key={client.id}>
+                        <th>{client.id}</th>
+                        <td>{client.name}</td>
+                        <td>{client.email}</td>
+                        <td>{client.job}</td>
+                        <td>{client.rate}</td>
+                        <td>
+                            <button className={`btn rounded-full w-20 ${client.isactive ? `btn-primary` : `btn-outline btn-primary` }`} >
+                                {client.isactive ? 'Active' : 'Inactive'}
+                            </button>
+                        </td>
+                        <td>
+                            <button className='btn btn-secundary'>Update</button>
+                        </td>
+
+                        <td>
+                            <button className='btn btn-accent'>Delete</button>
+                        </td>
+                    </tr>
+                ))};
+                </tbody>
+            </table>
+        </div>
+    )
+}
