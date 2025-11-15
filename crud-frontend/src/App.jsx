f9b20990f4b5943ab26e2c21d3b85bab8f98e9ce
maubri96 <rauzav96@outlook.es>
2025-11-15 09:10:00 -0600

    Instalacion de daisyUI-

--- crud-frontend/src/App.jsx
+++ crud-frontend/src/App.jsx
@@ -9,6 +9,9 @@ function App() {
       <h1 className='text-3xl font-bold underline'> 
         Hello Word!
       </h1>
+      <div>
+        <button className="btn btn-primary">Hola DaisyUI</button>
+      </div>
     </>
   )
 }
