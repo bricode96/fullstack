6fbd639c00a82700dd968372e1f8f1652643e50b
maubri96 <rauzav96@outlook.es>
2025-11-15 18:35:55 -0600

    Carpetas de Backend

--- crud-backend/src/index.js
+++ crud-backend/src/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express());
 
 app.get('/', (req, res) =>{
     res.send('<h1>Hello Backend</h1>');
